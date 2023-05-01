22e141de343d5074d9ea52387dc92b078c1a32e2
Kursat Aydemir <kursat@endpointdev.com>
2023-05-01 21:51:21 +0300

    Use synchronized HashMap

--- src/main/java/com/endpoint/SpringKafkaMessaging/websocket/WebSocketPool.java
+++ src/main/java/com/endpoint/SpringKafkaMessaging/websocket/WebSocketPool.java
@@ -2,12 +2,12 @@ package com.endpoint.SpringKafkaMessaging.websocket;
 
 import org.springframework.web.socket.WebSocketSession;
 
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class WebSocketPool {
 
-    public static Map<Long, Set<WebSocketSession>> websockets = new HashMap<>();
+    public static Map<Long, Set<WebSocketSession>> websockets = new ConcurrentHashMap<>();
 
 }
